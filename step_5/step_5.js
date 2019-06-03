f6f65fbdbf5ba1291004fab237bbc36975d314a3
nolangendron <nolanglengendron@gmail.com>
2019-06-03 19:29:37 +0000

    added module

---
+++ step_5/step_5.js
@@ -0,0 +1,14 @@
+
+var getHTML = require('./http-functions');
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+}
+
+getHTML(requestOptions, printHTML);
+
